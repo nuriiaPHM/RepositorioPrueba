8e961e89d563b80c7836ecfc56e9b2cfa5bc0dbf
a21nuriajm <a21nuriajm@192.168.56.1>
2022-05-09 09:07:49 +0200

    nuevo commit

--- ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
+++ ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
@@ -15,8 +15,7 @@ public class ProyectoPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        
+        System.out.println("Hola");
         
     }
     
